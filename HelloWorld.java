0388a21d27e7fa2d6f5d733eeca1b590faca3e9c
Vision0067 <143988451+Vision0067@users.noreply.github.com>
2023-09-04 16:57:09 +0530

    Create HelloWorld.java
    one file added

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        //craete variable
+        // Data Type var-name;
+        int rollNo;
+        
+        int age=21;
+        double price=105.55;
+        boolean isFree=true;
+        
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isFree);
+        System.out.println("Hello, World!");
+        System.out.println("learing java");
+    }
+}
